2f384db239445b8499f4558d52bf6859c96c1e24
Kabir Kapur <kabir@Kabirs-MacBook-Pro.local>
2024-10-20 17:21:22 -0700

    WIP: add non-functional boilerplate routes for events and profiles resources

---
+++ routes/profiles.ts
@@ -0,0 +1,23 @@
+import express, { Router } from "express";
+
+const router: Router = express.Router();
+
+router.get('/', () => {
+    
+});
+
+router.get('/', () => {
+
+});
+
+router.patch('/', () => {
+
+});
+
+router.post('/', () => {
+
+});
+
+router.delete('/', () => {
+
+});
